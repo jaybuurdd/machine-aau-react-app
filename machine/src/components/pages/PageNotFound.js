517436909cb80863339b5686af15ff6b1cfb862a
jobas <jobas@gmu.edu>
2022-04-29 20:50:34 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "thursdate""""""""""""""""""""""
    This reverts commit 2206d92f0d32f133028b007057e30be7d6c1d748.

---
+++ machine/src/components/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {MACHINEH2} from '../AppElements';
+
+function ErrorPage() {
+    return (
+        <div>
+            <MACHINEH2>That page does not exist</MACHINEH2>
+        </div>
+    )
+}
+
+export default ErrorPage
